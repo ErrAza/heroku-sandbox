087da7304106b021aeedd0ef6807c77315ba3eba
Sean <sean@seamonster.co.za>
2017-05-02 14:43:59 +0200

    Clean up

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.set('view engine', 'pug');
-app.set('views','./views');
-
 app.get('/', function(req, res){
-    //res.render('first_view');
-    res.send("Without Pug");
+    res.send("Pug Check!");
 });
 
 app.listen(3000);
